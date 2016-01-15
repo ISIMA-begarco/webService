12139219cdfe314aae4e83720f5c6c794eec65fb
Geoffrey Clauzier <gclauzier@gmail.com>
2016-01-15 22:35:18 +0100

    Ajoute la gestion des tournois dans le ModelView.

---
+++ ApplicationWPF/ViewModel/Tournament/TournamentsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiblioWPF.ViewModel;
+
+namespace ApplicationWPF.ViewModel.Tournament
+{
+    class TournamentsViewModel : ViewModelBase
+    {
+        private ObservableCollection<TournamentViewModel> m_tournaments;
+        
+        public ObservableCollection<TournamentViewModel> Tournaments
+        {
+            get { return m_tournaments; }
+            private set
+            {
+                m_tournaments = value;
+                OnPropertyChanged("Tournaments");
+            }
+        }
+
+        private TournamentViewModel m_selectedItem;
+
+        public TournamentViewModel SelectedItem
+        {
+            get { return m_selectedItem; }
+            set
+            {
+                m_selectedItem = value;
+                OnPropertyChanged("SelectedItem");
+            }
+        }
+
+        public TournamentsViewModel(IList<EntitiesLayer.Tournoi> tournamentModel)
+        {
+            m_tournaments = new ObservableCollection<TournamentViewModel>();
+            foreach (EntitiesLayer.Tournoi t in tournamentModel)
+            {
+                m_tournaments.Add(new TournamentViewModel(t));
+            }
+        }
+    }
+}
