c137b62687d7d89be76117b24df13670a8950755
Kami <lekamikaze03@hotmail.fr>
2016-01-21 18:31:42 +0100

    Modification de la fenêtre principale et création de la page principale. Ajout d'un UserControl controlant l'affichage des boutons de l'application.

---
+++ ApplicationWPF/AppButton1.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour AppButton1.xaml
+    /// </summary>
+    public partial class AppButton1 : UserControl
+    {
+        /// <summary>
+        /// Gets or sets the Label which is displayed next to the field
+        /// </summary>
+        public String Label
+        {
+            get { return (String)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        /// <summary>
+        /// Identified the Label dependency property
+        /// </summary>
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string),
+              typeof(AppButton1), new PropertyMetadata("Default"));
+
+        public AppButton1()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public event EventHandler Event;
+
+        private void UserControl1Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Event != null)
+            {
+                Event(this, new EventArgs());
+            }
+        }
+    }
+}
---
+++ ApplicationWPF/MainMenu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void AppButton1_Event(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
