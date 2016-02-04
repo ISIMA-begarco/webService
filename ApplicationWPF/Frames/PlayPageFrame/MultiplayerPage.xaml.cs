33998d8686019ad79d1c96a6bb86917dabf30e1e
Kami <lekamikaze03@hotmail.fr>
2016-02-04 19:53:57 +0100

    Mode 2 joueurs opérationnelle. Bouton fight qui tiens la route et Start beaucoup moins tolérant.

--- ApplicationWPF/Frames/PlayPageFrame/MultiplayerPage.xaml.cs
+++ ApplicationWPF/Frames/PlayPageFrame/MultiplayerPage.xaml.cs
@@ -34,8 +34,23 @@ namespace ApplicationWPF.Frames.PlayPageFrame
             IList<EntitiesLayer.Jedi> jedis = jtm.getJedis();
             ViewModel.Jedi.JedisModelView jvm = new ViewModel.Jedi.JedisModelView(jedis);
             J1Jedi.DataContext = jvm;
-            J2Jedi.DataContext = jvm;
 
+            IList<EntitiesLayer.Jedi> jedis2 = jtm.getJedis();
+            ViewModel.Jedi.JedisModelView jvm2 = new ViewModel.Jedi.JedisModelView(jedis);
+            J2Jedi.DataContext = jvm2;
+
+        }
+
+        private void J1Jedi_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if ((this.J1Jedi.ComboJedi.SelectedItem as ViewModel.Jedi.JediViewModel) != null)
+                BusinessLayer.PartieManager.getCurrentGame().Jedi_j1 = (this.J1Jedi.ComboJedi.SelectedItem as ViewModel.Jedi.JediViewModel).Jedi;
+        }
+
+        private void J2Jedi_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if ((this.J2Jedi.ComboJedi.SelectedItem as ViewModel.Jedi.JediViewModel) != null)
+                BusinessLayer.PartieManager.getCurrentGame().Jedi_j2 = (this.J2Jedi.ComboJedi.SelectedItem as ViewModel.Jedi.JediViewModel).Jedi;
         }
     }
 }
